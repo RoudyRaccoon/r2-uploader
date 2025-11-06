2f2535923097fa43d03aab9ea11aa5a5da799d23
RoudyRaccoon <roudy@anthroniagara.ca>
2025-11-05 19:03:46 -0500

    A

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
 const pinia = createPinia()
 
